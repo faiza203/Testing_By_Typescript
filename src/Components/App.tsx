20fd2cceeb7b1cf68dfba9c11081eec34dda773a
faiza203 <fiazfaiza981@gmail.com>
2020-11-14 13:34:48 +0500

    Add Timer in App.tsx

--- src/Components/App.tsx
+++ src/Components/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import Timer from './Timer/Timer';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      Hello World
+      <Timer />
     </div>
   );
 }
