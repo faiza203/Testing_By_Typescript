056ffa0357443be60531d9ae02c94a469bcfb68b
faiza203 <fiazfaiza981@gmail.com>
2020-11-14 13:30:48 +0500

    Create timer element

--- src/Components/Timer/Timer.tsx
+++ src/Components/Timer/Timer.tsx
@@ -1,1 +1,20 @@
-import React from 'react';
+import React, { Component } from 'react';
+
+class Timer extends Component {
+  constructor(props : any) {
+    super(props);
+    this.state = {
+      minutes: 25,
+      seconds: 0,
+ 	      isOn: false
+    };
+  }
+
+
+
+  render = () => {
+    return <div className="timer-container" />;
+  };
+}
+
+export default Timer;
