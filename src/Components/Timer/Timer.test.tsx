a62ac19232b960bc268558b8ffb6b8d37581adde
faiza203 <fiazfaiza981@gmail.com>
2020-11-14 13:39:05 +0500

    Test Timer in Timer.test.tsx

--- src/Components/Timer/Timer.test.tsx
+++ src/Components/Timer/Timer.test.tsx
@@ -1,1 +1,12 @@
 import React from 'react';
+import Timer from "./Timer";
+import { shallow } from "enzyme"
+describe("Timer", () => {
+  let container:any
+
+  beforeEach(() => (container = shallow(<Timer />)))
+
+  it("should render a <div />", () => {
+    expect(container.find("div").length).toBeGreaterThanOrEqual(1)
+  })
+})
