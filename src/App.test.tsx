3c675da5a6bea9b60f1982e96b817f4bad90620c
faiza203 <fiazfaiza981@gmail.com>
2020-11-14 12:49:45 +0500

    Update App.test.tsx

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { shallow } from 'enzyme';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('App', () => {
+	it('should render a <div />', () => {
+		const container = shallow(<App />);
+		expect(container.find('div').length).toEqual(1);
+});
 });
