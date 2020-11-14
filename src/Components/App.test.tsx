36c9724af250a1bdb297afb5aed14f54adff88f4
faiza203 <fiazfaiza981@gmail.com>
2020-11-14 13:36:28 +0500

    Test Timer in App.test.tsx

--- src/Components/App.test.tsx
+++ src/Components/App.test.tsx
@@ -1,10 +1,18 @@
-import React from 'react';
-import { shallow } from 'enzyme';
-import App from './App';
+import React from "react"
+import { shallow } from "enzyme"
+import App from "./App"
+import Timer from "./Timer/Timer"
 
-describe('App', () => {
-	it('should render a <div />', () => {
-		const container = shallow(<App />);
-		expect(container.find('div').length).toEqual(1);
-});
-});
+describe("App", () => {
+  let container:any
+
+  beforeEach(() => (container = shallow(<App />)))
+
+  it("should render a <div />", () => {
+    expect(container.find("div").length).toEqual(1)
+  })
+
+  it("should render the Timer Component", () => {
+    expect(container.containsMatchingElement(<Timer />)).toEqual(true)
+  })
+})
